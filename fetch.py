8de4804a33d3232d286dc7376aa83f2262d535b0
BDL <13621351+0xlino@users.noreply.github.com>
2023-12-07 12:34:13 +0000

    Make changes to profile repo

---
+++ fetch.py
@@ -0,0 +1,35 @@
+import requests
+
+def add_banner_to_top():
+    with open('README.md', 'a') as f:
+        f.truncate(0)
+        f.write("<img src=\"https://github-profile-trophy.vercel.app/?username=0xlino&theme=onedark\"/>\n")
+        f.write("\n")
+
+def get_github_public_repos_of_user(username):            
+    response = requests.get('https://api.github.com/users/' + username + '/repos')
+    if response.status_code == 200:
+        data = response.json()
+        sorted_data = sorted(data, key=lambda x: x.get("pushed_at", ""), reverse=True)
+
+        # dump nice json to file
+        # with open('data.json', 'w') as f:
+        #     json.dump(sorted_data, f, indent=4)
+
+        with open('README.md', 'a') as f:
+            f.write("### Public Repositories \n")
+            for repo in sorted_data:
+                # f.write("- <samp>[" + repo['name'] + "](" + repo['html_url'] + ") <kbd>" + repo['updated_at'] + "</kbd></samp>\n")
+                # f.write("- [" + repo['name'] + "](" + repo['html_url'] + ")\n")
+                # if repo['description'] is None: 
+                # if repo['name'] == '0xlino' skip it
+                if repo['name'] == '0xlino':
+                    continue
+                if repo['description'] is None: 
+                    repo['description'] = 'No description provided'
+                f.write("- [" + repo['name'] + "](" + repo['html_url'] + ") - " + repo['description'] + "\n")
+    else:
+        print('Error')
+
+add_banner_to_top()
+get_github_public_repos_of_user('0xlino')
